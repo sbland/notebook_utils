914bd7d32954cc0a272f26d05b8b12e491d8ef49
Sam Bland <sbland.co.uk@gmail.com>
2021-11-25 16:45:11 +0000

    Update colab script

--- notebook_utils/collab.py
+++ notebook_utils/collab.py
@@ -1,3 +1,5 @@
+
+import importlib
 def collab_install_git_dependencies(
     DEPENDENCIES,
 ) -> str:
@@ -7,44 +9,47 @@ def collab_install_git_dependencies(
     -------
     DEPENDENCIES=[
         ("name", "github.com/user/repo.git@RELEASE"),
+        ("notebook_utils", f"github.com/sbland/notebook_utils/archive/main.zip"),
     ]
     """
-    try:
-        import importlib
-        for name, path in DEPENDENCIES:
+    for name, path in DEPENDENCIES:
+        try:
             importlib.import_module(name)
-    except (ImportError, ModuleNotFoundError):
-        if 'google.colab' in str(get_ipython()):
-            print('Running on CoLab')
-            print("""
-            This notebook requires access to private repositories. To access follow the steps below:
-            1. Get an access key from your github account: https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token
-            2. Save the key to a file in the following location on your google drive: `My Drive/access/collabaccess.txt`. The file should include your user on the first line and your token on the second.
-            """)
-            from google.colab import drive
-            drive.mount('/content/drive')
-
-            !mkdir -p ~/.access
-            !cp "/content/drive/My Drive/access/collabaccess.txt" ~/.access/config
-            import os
-            creds = open(f'{os.path.expanduser("~")}/.access/config')
-            creds_parsed = creds.read().splitlines()
-            user, token = creds_parsed
-            for name, path in DEPENDENCIES:
-                print(f"------ Installing {name}----------------")
+        except (ImportError, ModuleNotFoundError):
+            if 'google.colab' in str(get_ipython()):
                 try:
-                    stdout = !pip install git+https://{user}:{token}@{path}
-                    if "Host key verification failed" in "\n".join(stdout):
-                        print(f"------------- Failed to install with dependencies for {name} ---------------")
-                        # If it fails to install try installing without dependencies
-                        # git+ssh dependencies will currently fail
-                        !pip install git+https://{user}:{token}@{path} --no-deps
+                    print('Running on CoLab')
+                    print("""
+                    This notebook requires access to private repositories. To access follow the steps below:
+                    1. Get an access key from your github account: https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token
+                    2. Save the key to a file in the following location on your google drive: `My Drive/access/collabaccess.txt`. The file should include your user on the first line and your token on the second.
+                    """)
+                    from google.colab import drive
+                    drive.mount('/content/drive')
+
+                    !mkdir -p ~/.access
+                    !cp "/content/drive/My Drive/access/collabaccess.txt" ~/.access/config
+                    import os
+                    creds = open(f'{os.path.expanduser("~")}/.access/config')
+                    creds_parsed = creds.read().splitlines()
+                    user, token = creds_parsed
+
+                    print(f"------ Installing {name}----------------")
+                    try:
+                        stdout = !pip install https://{user}:{token}@{path} --ignore-requires-python
+                        if "Host key verification failed" in "\n".join(stdout):
+                            print(f"------------- Failed to install with dependencies for {name} ---------------")
+                            # If it fails to install try installing without dependencies
+                            # git+ssh dependencies will currently fail
+                            !pip install https://{user}:{token}@{path} --no-deps --ignore-requires-python
+                    except:
+                        print(f"Failed to install all of {name} at {path}")
+                    finally:
+                        print("\n".join(stdout))
+                    creds.close()
                 except:
-                    print(f"Failed to install all of {name} at {path}")
-                finally:
-                    print("\n".join(stdout))
-            creds.close()
-            creds = None
-            creds_parsed = None
-            user = None
-            token = None
+                    print("=======FAILED TO INSTALL ADDTIONAL DEPENDENCIES!=======\nCheck instructions above")
+    creds = None
+    creds_parsed = None
+    user = None
+    token = None
